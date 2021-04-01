8948358690a4c57409c88f1d1c06dd12d480a031
krishpranav <krisna.pranav@gmail.com>
2021-04-01 10:45:41 +0530

    done changes

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+### IMPORTS ###
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
